09a945584260a3665d8b7b356b476466b14b4aea
Anthony van Deventer <anthony.vandeventer@standardbank.co.za>
2021-09-25 09:54:15 +0200

    Initialization of spring boot Restful project

---
+++ src/main/java/van/deventer/IoCPractice/IoCPracticeApplication.java
@@ -0,0 +1,13 @@
+package van.deventer.IoCPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoCPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoCPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/van/deventer/IoCPractice/IoCPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package van.deventer.IoCPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoCPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
